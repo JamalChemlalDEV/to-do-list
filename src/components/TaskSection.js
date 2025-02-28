2efe0b152e19488ff047a87886388827a0327c3a
Jamal Chamlal <jamalchamlal51@gmail.com>
2025-02-28 21:34:47 +0000

    Update TaskSection.js

--- src/components/TaskSection.js
+++ src/components/TaskSection.js
@@ -5,7 +5,7 @@ function TaskSection(props) {
     
     return(
         <div class="section-task">
-            <h3>Estas son tus Tareas Pendientes </h3>
+            <h3>Da sind deine ausstehenden Aufgaben</h3>
             <ul>
                 {props.taskItems.map( (task, index) => {
                     return(
